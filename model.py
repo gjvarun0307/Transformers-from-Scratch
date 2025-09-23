8bc2099224dacfa065fbd4b88c36d768c915d9d5
Varun Balan G <gjvarun0307@gmail.com>
2025-09-22 22:20:55 -0500

    Feed Forward

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import math
 
+# Input Embedding Layer
 class InputEmbedding(nn.Module):
 
     def __init__(self, d_model: int, vocab_size: int):
@@ -12,7 +13,8 @@ class InputEmbedding(nn.Module):
 
     def forward(self, x):
         return self.embedding(x) * math.sqrt(self.d_model)
-    
+
+# Positional Encoding Layer   
 class PositionalEncoding(nn.Module):
 
     def __init__(self, d_model: int, seq_len: int, dropout: float):
@@ -21,4 +23,47 @@ class PositionalEncoding(nn.Module):
         self.seq_len = seq_len
         self.dropout = nn.Dropout(dropout)
 
+        # create matrix of size (seq_len, d_model)
+        pe = torch.zeros(seq_len, d_model)
         
+        # create a vector of size (seq_len)
+        position = torch.arange(0, seq_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)    # even indices have sine
+        pe[:, 1::2] = torch.cos(position * div_term)    # odd indices have cosine
+
+        pe = pe.unsqueeze(0)  # add batch dimension
+
+        self.register_buffer('pe', pe)  # register as buffer so it is not a parameter but gets saved in the state dict and reused
+    
+    def forward(self, x):
+        x = x + (self.pe[:, :x.shape[1], :]).requires_grad_(False)
+        return self.dropout(x)
+    
+# Layer Normalization
+class LayerNorm(nn.Module):
+
+    def __init__(self, eps: float = 1e-6):
+        super().__init__()
+        self.eps = eps
+        self.alpha = nn.Parameter(torch.ones(1))
+        self.bias = nn.Parameter(torch.zeros(1))
+    
+    def forward(self, x):
+        mean = x.mean(-1, keepdim=True)
+        std = x.std(-1, keepdim=True)
+        return self.alpha * (x - mean) / (std + self.eps) + self.bias
+
+# Feed Forward Layer
+class FeedForward(nn.Module):
+
+    def __init__(self, d_model:int, dff:int, dropout:float=0.1):
+        super().__init__()
+        self.linear1 = nn.Linear(d_model, dff)
+        self.linear2 = nn.Linear(dff, d_model)
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.linear2(self.dropout(torch.relu(self.linear1(x))))
+    
+# Multi-Head Attention Layer
