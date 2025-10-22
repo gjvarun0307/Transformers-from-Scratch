d7f73ad2843f2eb41f77f0e2071bef263b46ef9d
Varun Balan G <gjvarun0307@gmail.com>
2025-10-22 17:01:31 -0500

    fixed bugs

--- train.py
+++ train.py
@@ -157,6 +157,7 @@ def train_model(config):
         model_filename = get_weights_file_path(config, config['preload'])
         print(f"preloading model {model_filename}")
         state = torch.load(model_filename)
+        model.load_state_dict(state['model_state_dict'])
         inital_epoch = state['epoch'] + 1
         optimizer.load_state_dict(state['optimizer_state_dict'])
         global_step = state['global_step']
